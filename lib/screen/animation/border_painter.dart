d6b516a56a75ad8fc1ad180c06c82df84a976b75
cuongceg <domanhcuong2004thptahhnd@gmail.com>
2024-08-06 16:32:18 +0700

    refactor: change the shape of border

--- lib/screen/animation/border_painter.dart
+++ lib/screen/animation/border_painter.dart
@@ -1,32 +1,61 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 
 class BorderPainter extends CustomPainter {
-  final double currentState;
+  final double controller;
 
-  BorderPainter({required this.currentState});
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return true;
-  }
+  BorderPainter(this.controller);
 
   @override
   void paint(Canvas canvas, Size size) {
+    double sh = size.height; // For path shortage
+    double sw = size.width;  // For path shortage
+    double line = 30.0;  // Length of the animated line
+    double c1 = controller * 2; // Controller value for top and left border.
+    double c2 = controller >= 0.5 ? (controller - 0.5) * 2 : 0; // Controller value for bottom and right border.
 
-    double strokeWidth = 4;
-    Rect rect = const Offset(0,0) & Size(size.width, size.height);
+    Paint paint = Paint()
+      ..color = Colors.blue
+      ..strokeWidth = 8
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round;
 
-    var paint = Paint()
-      ..color = Colors.blueAccent
-      ..strokeWidth = strokeWidth
-      ..style = PaintingStyle.stroke;
+    Path top = Path()
+      ..moveTo(10, 0)
+      ..lineTo(sw * c1 + line >= sw ? sw : sw * c1 + line, 0);
 
-    double startAngle = pi / 2;
-    double sweepAmount = currentState * pi;
+    Path left = Path()
+      ..lineTo(0, sh * c1 + line >= sh ? sh : sh * c1 + line);
 
-    canvas.drawArc(rect, startAngle, sweepAmount, false, paint);
-    canvas.drawArc(rect, startAngle, -sweepAmount, false, paint);
+    Path right = Path()
+      ..moveTo(sw,0)
+      ..lineTo(
+          sw,
+          sh * c2 + line > sh
+              ? sh
+              : sw * c1 + line >= sw
+              ? sw * c1 + line - sw
+              : sh * c2);
+
+    Path bottom =  Path()
+      ..moveTo(0, sh)
+      ..lineTo(
+          sw * c2 + line > sw
+              ? sw
+              : sh * c1 + line >= sh
+              ? sh * c1 + line - sh
+              : sw * c2, sh);
+
+
+
+    canvas.drawPath(top, paint);
+    canvas.drawPath(left, paint);
+    canvas.drawPath(right, paint);
+    canvas.drawPath(bottom, paint);
   }
 
+  @override
+  bool shouldRepaint(BorderPainter oldDelegate) => true;
+
+  @override
+  bool shouldRebuildSemantics(BorderPainter oldDelegate) => false;
 }
